6847e61c307506cd334dafe19ee72098b394661a
shockinglee <shockingli@126.com>
2014-04-18 16:27:33 +0800

    remove tmp files

--- angular part-one template/js/controller/HelpOrderingController.js
+++
@@ -1,8 +0,0 @@
-function HelpOrderingController($scope, $navigate) {
-
-    //*****************在这里添加你的代码*****************
-
-
-
-
-}
--- angular part-one template/js/controller/HomePageController.js
+++
@@ -1,7 +0,0 @@
-function HomePageController($scope, $navigate) {
-
-    $scope.go_help_ordering_page = function () {
-        $navigate.go('/help_ordering_page')
-    }
-
-}
